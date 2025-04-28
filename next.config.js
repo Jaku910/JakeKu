49ff1cb7e2e7db4b73fc3ee0ae8f9a423e3cb5de
Jake Ku <jaku910@uw.edu>
2025-04-28 12:40:17 -0700

    Fix deployment configuration

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
   // Since we're doing static export, we need to disable features that require a Node.js server
   trailingSlash: true, // This helps with static hosting
   basePath: '/JakeKu',
+  // Disable features that require a Node.js server
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, path: false };
+    return config;
+  },
 }
 
 module.exports = nextConfig 
